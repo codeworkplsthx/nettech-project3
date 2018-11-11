2560a9a375e213ba19bcc09a16d255a9800196b0
Duncan Graham <dcgraham1282@gmail.com>
2018-11-11 09:38:12 -0500

    added exception handling to client

--- client.py
+++ client.py
@@ -1,33 +1,38 @@
 import socket
-
+import sys
 from table import *
 
 # client program
 
 
 
-# what the client sends to the server
-hostnames = read_hns_table("PROJ2-HNS.txt")
-
-# Define the port on which you want to connect to the server
-PORT = 65000
-# get local hostname
-HOST = socket.gethostbyname('localhost')
 
 
 # connect to RS server
 
 def main():
+    # what the client sends to the server
+    hostnames = read_hns_table(sys.argv[2])
+
+    # Define the port on which you want to connect to the server
+    PORT = 65000
+    # get local hostname
+    HOST = socket.gethostbyname(sys.argv[1])
+
     buffer = str()
     for h in hostnames:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as rs:
-            rs.connect((HOST, PORT))
-            rs.sendall(str.encode(h, 'utf-8'))
-            resp = DNSEntry.from_str(bytes.decode(rs.recv(1024), 'utf-8'))
-            if resp.qtype == '-':
-                buffer = buffer + str(h) + " " + "ERROR: HOST NOT FOUND\n"
-            else:
-                buffer = buffer + str(h) + " " + str(resp.ip) + " " + str(resp.qtype) + "\n"
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as rs:
+                rs.connect((HOST, PORT))
+                rs.sendall(str.encode(h, 'utf-8'))
+                resp = DNSEntry.from_str(bytes.decode(rs.recv(1024), 'utf-8'))
+                if resp.qtype == '-':
+                    buffer = buffer + str(h) + " " + "ERROR: HOST NOT FOUND\n"
+                else:
+                    buffer = buffer + str(h) + " " + str(resp.ip) + " " + str(resp.qtype) + "\n"
+        except ConnectionRefusedError:
+            print("run rs.py first")
+            break
 
 
     with open("RESOLVED.txt", 'w') as file:
